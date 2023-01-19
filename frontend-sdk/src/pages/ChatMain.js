dbf377fa7cf9fccc51779507670ff9695668341b
tgashwinkumar <tgashwinkumar@gmail.com>
2023-01-19 23:00:11 +0530

    Update

---
+++ frontend-sdk/src/pages/ChatMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatMain = () => {
+  return (
+    <main className="h-screen w-screen bg-[#161C23] px-[calc(100vw/12)] py-8">
+      <section className="bg-[#161C23] rounded-md w-full h-full"></section>
+    </main>
+  );
+};
+
+export default ChatMain;
